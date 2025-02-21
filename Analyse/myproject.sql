145bfcef0ee6ad8994ee7509b1fc507672eef979
Stagiaire-Jamila <akouranejamilatsdi@gmail.com>
2025-02-21 20:22:23 +0100

    modifier cmd

--- Analyse/myproject.sql
+++ Analyse/myproject.sql
@@ -16,29 +16,9 @@ CREATE SCHEMA IF NOT EXISTS `myproject` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 USE `myproject` ;
 
 -- -----------------------------------------------------
--- Table `myproject`.`Patients`
+-- Table `myproject`.`Medecins`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `myproject`.`Patients` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(100) NOT NULL,
-  `prenom` VARCHAR(100) NOT NULL,
-  `naissance` DATE NULL,
-  `adresse` VARCHAR(255) NULL,
-  `telephone` VARCHAR(15) NULL,
-  `email` VARCHAR(100) NULL,
-  `observation` TEXT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-CREATE UNIQUE INDEX `telephone_UNIQUE` ON `myproject`.`Patients` (`telephone` ASC) VISIBLE;
-
-CREATE UNIQUE INDEX `email_UNIQUE` ON `myproject`.`Patients` (`email` ASC) VISIBLE;
-
-
--- -----------------------------------------------------
--- Table `myproject`.`Médecins`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `myproject`.`Médecins` (
+CREATE TABLE IF NOT EXISTS `myproject`.`Medecins` (
   `id` INT NOT NULL,
   `nom` VARCHAR(100) NOT NULL,
   `prenom` VARCHAR(100) NOT NULL,
@@ -48,9 +28,21 @@ CREATE TABLE IF NOT EXISTS `myproject`.`Médecins` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `telephone_UNIQUE` ON `myproject`.`Médecins` (`telephone` ASC) VISIBLE;
+CREATE UNIQUE INDEX `telephone_UNIQUE` ON `myproject`.`Medecins` (`telephone` ASC) VISIBLE;
 
-CREATE UNIQUE INDEX `email_UNIQUE` ON `myproject`.`Médecins` (`email` ASC) VISIBLE;
+CREATE UNIQUE INDEX `email_UNIQUE` ON `myproject`.`Medecins` (`email` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `myproject`.`total_tarifs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `myproject`.`total_tarifs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `frais` VARCHAR(45) NOT NULL,
+  `tarif` FLOAT(6,2) NOT NULL,
+  `montant total` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -62,43 +54,54 @@ CREATE TABLE IF NOT EXISTS `myproject`.`Rendez_vous` (
   `date_rdv` VARCHAR(45) NULL,
   `heur_rdv` DATETIME NULL,
   `reciption` TIME NULL,
-  `Médecins_id` INT NOT NULL,
-  `Patients_id` INT NOT NULL,
+  `date_paiements` DATETIME NOT NULL,
+  `pays` VARCHAR(3) NULL,
+  `Medecins_id` INT NOT NULL,
+  `total_tarifs_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_Rendez-vous_Médecins`
-    FOREIGN KEY (`Médecins_id`)
-    REFERENCES `myproject`.`Médecins` (`id`)
+  CONSTRAINT `fk_Rendez_vous_Medecins`
+    FOREIGN KEY (`Medecins_id`)
+    REFERENCES `myproject`.`Medecins` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Rendez_vous_Patients1`
-    FOREIGN KEY (`Patients_id`)
-    REFERENCES `myproject`.`Patients` (`id`)
+  CONSTRAINT `fk_Rendez_vous_total_tarifs1`
+    FOREIGN KEY (`total_tarifs_id`)
+    REFERENCES `myproject`.`total_tarifs` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Rendez-vous_Médecins_idx` ON `myproject`.`Rendez_vous` (`Médecins_id` ASC) VISIBLE;
+CREATE INDEX `fk_Rendez_vous_Medecins_idx` ON `myproject`.`Rendez_vous` (`Medecins_id` ASC) VISIBLE;
 
-CREATE INDEX `fk_Rendez_vous_Patients1_idx` ON `myproject`.`Rendez_vous` (`Patients_id` ASC) VISIBLE;
+CREATE INDEX `fk_Rendez_vous_total_tarifs1_idx` ON `myproject`.`Rendez_vous` (`total_tarifs_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
--- Table `myproject`.`tarifs`
+-- Table `myproject`.`Patients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `myproject`.`tarifs` (
+CREATE TABLE IF NOT EXISTS `myproject`.`Patients` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `frais` VARCHAR(45) NOT NULL,
-  `tarif` FLOAT(6,2) NOT NULL,
-  `Médecins_id` INT NOT NULL,
+  `nom` VARCHAR(100) NOT NULL,
+  `prenom` VARCHAR(100) NOT NULL,
+  `naissance` DATE NULL,
+  `adresse` VARCHAR(255) NULL,
+  `telephone` VARCHAR(15) NULL,
+  `email` VARCHAR(100) NULL,
+  `observation` TEXT NULL,
+  `Rendez_vous_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_tarifs_Médecins1`
-    FOREIGN KEY (`Médecins_id`)
-    REFERENCES `myproject`.`Médecins` (`id`)
+  CONSTRAINT `fk_Patients_Rendez_vous1`
+    FOREIGN KEY (`Rendez_vous_id`)
+    REFERENCES `myproject`.`Rendez_vous` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_tarifs_Médecins1_idx` ON `myproject`.`tarifs` (`Médecins_id` ASC) VISIBLE;
+CREATE UNIQUE INDEX `telephone_UNIQUE` ON `myproject`.`Patients` (`telephone` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `email_UNIQUE` ON `myproject`.`Patients` (`email` ASC) VISIBLE;
+
+CREATE INDEX `fk_Patients_Rendez_vous1_idx` ON `myproject`.`Patients` (`Rendez_vous_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -108,24 +111,17 @@ CREATE TABLE IF NOT EXISTS `myproject`.`Frais` (
   `id` INT NOT NULL,
   `type-frais` VARCHAR(100) NULL,
   `statut` VARCHAR(100) NULL,
-  `tarifs_id` INT NOT NULL,
-  `Rendez_vous_id` INT NOT NULL,
+  `tarifs` FLOAT(6,2) NULL,
+  `total_tarifs_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_Frais_tarifs1`
-    FOREIGN KEY (`tarifs_id`)
-    REFERENCES `myproject`.`tarifs` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Frais_Rendez_vous1`
-    FOREIGN KEY (`Rendez_vous_id`)
-    REFERENCES `myproject`.`Rendez_vous` (`id`)
+  CONSTRAINT `fk_Frais_total_tarifs1`
+    FOREIGN KEY (`total_tarifs_id`)
+    REFERENCES `myproject`.`total_tarifs` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_Frais_tarifs1_idx` ON `myproject`.`Frais` (`tarifs_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_Frais_Rendez_vous1_idx` ON `myproject`.`Frais` (`Rendez_vous_id` ASC) VISIBLE;
+CREATE INDEX `fk_Frais_total_tarifs1_idx` ON `myproject`.`Frais` (`total_tarifs_id` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
